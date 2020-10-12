e7f65e2b0f5e772505686accba96ce27fc797f03
dbstjr5517 <dbstjr5517@naver.com>
2020-10-12 12:06:59 +0900

    10/12 12:06
    저장불러오기, 사운드 마무리

--- Content/Scripts/typings/_part_1_ue.d.ts
+++ Content/Scripts/typings/_part_1_ue.d.ts
@@ -4979,7 +4979,7 @@ declare class BlueprintEditorSettings extends UObject {
 	bHideConstructionScriptComponentsInDetailsView: boolean;
 	bHostFindInBlueprintsInGlobalTab: boolean;
 	bNavigateToNativeFunctionsFromCallNodes: boolean;
-	Bookmarks: any;
+	BookMarks: any;
 	BookmarkNodes: BPEditorBookmarkNode[];
 	bIncludeCommentNodesInBookmarksTab: boolean;
 	bShowBookmarksForCurrentDocumentOnlyInTab: boolean;
